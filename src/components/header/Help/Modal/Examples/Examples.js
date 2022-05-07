f2d804a600d162efa6c94b1930a9381418ab8bfd
Jacob Anderson <1anderson2jacob@gmail.com>
2022-05-06 17:38:16 -0700

    fixed typo in help modal

--- src/components/header/Help/Modal/Examples/Examples.js
+++ src/components/header/Help/Modal/Examples/Examples.js
@@ -27,7 +27,7 @@ function Examples() {
       <ul>
         <li><div data-state="absent">W</div> & <div data-state="absent">Y</div> show the letters are <em>incorrect</em> and have been clicked 0 times</li>
         <li><div data-state="correct">O</div> shows the letter is <em>correct</em>, is in the <em>correct</em> position, and has been clicked 2 times</li>
-        <li><div data-state="present">R</div> & <div data-state="present">Y</div> show the letters are <em>correct</em>, are in the <em>incorrect</em> position, and have been clicked 1 time</li>
+        <li><div data-state="present">R</div> & <div data-state="present">M</div> show the letters are <em>correct</em>, are in the <em>incorrect</em> position, and have been clicked 1 time</li>
       </ul>
     </div>
   )
