33947da465e15e8fec55237e45099d3c0020060b
Jacob Anderson <1anderson2jacob@gmail.com>
2022-05-09 16:48:03 -0700

    1) Stopped scrolling while using modal 2) Fixed loading and error not showing up when fetching data3) Fixed page height not taking into account address bars on mobile 4) Fixed mobile input selection 5) Set url to .../solvre 6) Added page title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { useEffect } from 'react'
 
 import Header from './components/Header/Header';
 import Board from './components/Board/Board'
 
 
 function App() {
+	useEffect(() => {
+		document.title = "Solvre - a solution to Wordle"
+	}, [])
+	
   return (
 		<>
 			<Header></Header>
